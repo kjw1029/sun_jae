7ead2b1e41746687d92fd1b3e3aa02fc1fa7cc73
kang <kang@DESKTOP-2UCP4GJ>
2023-12-30 23:39:31 +0900

    깃에서 브랜치 병합테스트

--- 231230 gitHub/src/Hotel.java
+++ 231230 gitHub/src/Hotel.java
@@ -6,5 +6,6 @@ public class Hotel {
 		System.out.println("브랜치 생성 ");
 		System.out.println("흠...병합은된거같은데 내 컴퓨터 안에 이상이 있나?");
 		System.out.println("진짜진짜 마지막 테스트");
+		System.out.println("깃에서 합치기");
 	}
 }
